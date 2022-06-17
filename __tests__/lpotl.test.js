e2704b1885cd803f4af51485fd0e70c6f653dab1
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-17 12:11:17 -0700

    update and delete tests passing

--- __tests__/lpotl.test.js
+++ __tests__/lpotl.test.js
@@ -34,4 +34,24 @@ describe('lpotl routes', () => {
       'Clearly the best host of Last Podcast on the Left and its not even close.'
     );
   });
+  it('PUT /lpotl/id should update a cast member', async () => {
+    const resp = await request(app)
+      .put('/lpotl/1')
+      .send({ name: 'Ben the ben Kissel kissel (kissel)' });
+    expect(resp.status).toEqual(200);
+    expect(resp.body.name).toEqual('Ben the ben Kissel kissel (kissel)');
+  });
+});
+it('delete /alien/:id should delete an alien', async () => {
+  const resp = await request(app).delete('/aliens/1');
+  expect(resp.status).toEqual(200);
+
+  const response = await request(app).get('/aliens/1');
+  expect(response.body).toEqual({
+    status: 500,
+    message: "Cannot read properties of undefined (reading 'id')",
+  });
+});
+afterAll(() => {
+  pool.end();
 });
