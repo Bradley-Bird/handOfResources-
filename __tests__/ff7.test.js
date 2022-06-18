606d9a6ccef47f05079df46ed07508efca1b840e
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-17 17:30:28 -0700

    update and delete tests passing

--- __tests__/ff7.test.js
+++ __tests__/ff7.test.js
@@ -23,7 +23,7 @@ describe('ff7 routes', () => {
         'Former First Class SOLDIER. After defecting from Shinra, Cloud began work as a mercenary for hire in Midgar. With his trusty broadsword in hand, he always gets the job done.',
     });
   });
-  it('PUT inserts a new ff7 character', async () => {
+  it('post inserts a new ff7 character', async () => {
     const resp = await request(app).post('/ff7').send({
       name: 'Sephiroth',
       about: 'evil bad one wing angel',
@@ -31,6 +31,20 @@ describe('ff7 routes', () => {
     expect(resp.status).toEqual(200);
     expect(resp.body.name).toEqual('Sephiroth');
     expect(resp.body.about).toEqual('evil bad one wing angel');
+  });
+  it('put updates on /ff7/id', async () => {
+    const resp = await request(app)
+      .put('/ff7/1')
+      .send({ name: 'Cloudy mcCloudCloud' });
+    expect(resp.status).toEqual(200);
+    expect(resp.body.name).toEqual('Cloudy mcCloudCloud');
+  });
+  it('delete /ff7/:id should delete a character', async () => {
+    const resp = await request(app).delete('/ff7/1');
+    expect(resp.status).toEqual(200);
+
+    const response = await request(app).get('/ff7/1');
+    expect(response.body).toEqual('');
   });
   afterAll(() => {
     pool.end();
